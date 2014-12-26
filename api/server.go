88db2bdad8a56cfc2f1e4aa649ae605afe175c2c
Freddy Kelly <fred@bitnami.com>
2014-12-26 18:10:25 +0000

    basic checking of facebook access token

--- api/server.go
+++ api/server.go
@@ -2,50 +2,42 @@ package main
 
 import (
   "os"
-  "fmt"
+  "log"
+  //"fmt"
   "net/http"
 
   "github.com/go-martini/martini"
-  "github.com/martini-contrib/sessions"
-  "github.com/markbates/goth"
-  "github.com/markbates/goth/gothic"
-  "github.com/markbates/goth/providers/facebook"
+  fb "github.com/huandu/facebook"
 )
 
 func main() {
-
-  goth.UseProviders(
-    facebook.New(os.Getenv("FACEBOOK_KEY"), os.Getenv("FACEBOOK_SECRET"), "http://localhost:3000/auth/facebook/callback"),
-  )
-
-  gothic.GetProviderName = func(req *http.Request) (string, error) {
-    return "facebook", nil
-  }
+  // create a global App var to hold your app id and secret.
+  var globalApp = fb.New(os.Getenv("FACEBOOK_KEY"), os.Getenv("FACEBOOK_SECRET"))
+  globalApp.RedirectUri = "http://localhost:3000/auth/facebook/callback"
 
   // instantiate Martini
   m := martini.Classic()
+  m.Use(martini.Logger())
 
   m.Use(func(res http.ResponseWriter, req *http.Request) {
-    if 1 == 2 {
+    _, err := fb.Get("/me", fb.Params{
+      "access_token": req.Header.Get("Authorization"),
+    })
+
+    if err != nil {
+      // err can be an facebook API error.
+      // if so, the Error struct contains error details.
+      if e, ok := err.(*fb.Error); ok {
+        log.Printf("facebook error. [message:%v] [type:%v] [code:%v] [subcode:%v]", e.Message, e.Type, e.Code, e.ErrorSubcode)
+      }
+
       res.WriteHeader(http.StatusUnauthorized)
     }
   })
 
-  m.Get("/", func(session sessions.Session) string {
-    //user, err := gothic.FetchUser()
-    //return fmt.Sprintf("Hello world!, access_token=%s", token.(string))
+  m.Get("/", func() string {
     return "Hello world!"
   })
-
-  m.Get("/auth/:provider/callback", func(params martini.Params, res http.ResponseWriter, req *http.Request) string {
-    user, err := gothic.CompleteUserAuth(res, req)
-    if err != nil {
-      return "Something went wrong."
-    }
-    return fmt.Sprintf("Logged in as %s, access_token=%s", user.Name, user.AccessToken)
-  })
-
-  m.Get("/auth/:provider", gothic.BeginAuthHandler)
 
   m.Run()
 }
