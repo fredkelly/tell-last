1638fd07c09ce2b8b473db16fa09220c3e978f2a
Freddy Kelly <fred@bitnami.com>
2014-12-26 23:32:30 +0000

    basic facebook client impl

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -3,17 +3,64 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('tellLast', ['ionic'])
+angular.module('tellLast', ['ionic', 'ngFacebook'])
 
-.run(function($ionicPlatform) {
+.config(function($stateProvider, $urlRouterProvider, $facebookProvider) {
+  $facebookProvider
+    .setAppId('1390390824592299')
+    .setCustomInit({ version: 'v1.0' })
+    //.setVersion('v2.1');
+
+  $urlRouterProvider.otherwise('/')
+
+  $stateProvider.state('inbox', {
+    url: '/',
+    views: {
+      inbox: { templateUrl: 'inbox.html' }
+    }
+  })
+
+  $stateProvider.state('new', {
+    url: '/new',
+    views: {
+      new: { templateUrl: 'new.html' }
+    },
+    requireLogin: true
+  })
+})
+
+.run(function($ionicPlatform, $rootScope, $facebook) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
+    }
+  });
+
+  // Facebook SDK
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+  $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
+    if (toState.requireLogin && typeof $rootScope.currentUser === 'undefined') {
+      event.preventDefault();
+      var response = $facebook.getAuthResponse();
+      console.log(response);
+      //$facebook.api('/me').then(function(user) {
+      //  console.log(user);
+      //},
+      //function(error) {
+      //  console.log(error);
+      //});
     }
   });
 })
