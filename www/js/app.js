2e2b99cf3ce3e38a08d913cd692d8ae188cfe718
Freddy Kelly <me@fredkelly.net>
2014-12-26 10:49:54 +0000

    adds basic markup for tell/inbox views

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('tellLast', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -16,4 +16,22 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+})
+
+.controller('TellCtrl', function($scope) {
+  $scope.friends = [
+    { id: 1, name: 'Joe Bloggs' },
+    { id: 2, name: 'Dave Smith' }
+  ];
+
+  $scope.body = '';
+  $scope.from = $scope.friends[0];
+  $scope.to = $scope.friends[1];
+})
+
+.controller('InboxCtrl', function($scope) {
+  $scope.items = [
+    { created: new Date(), title: 'Test item', body: 'Some lovely thoughts about said person.', uuid: '656691226' },
+    { created: new Date(), title: 'Secret Complement', body: 'More lovely thoughts about said person.', uuid: '590344115'}
+  ];
 })
